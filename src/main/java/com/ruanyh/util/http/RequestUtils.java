f1c755cff46b2c5d118fe8a68407343d2ba1c4a4
Ruanyh <ruanyhmail@gmail.com>
2017-03-14 16:54:36 +0800

    commit

---
+++ src/main/java/com/ruanyh/util/http/RequestUtils.java
@@ -0,0 +1,11 @@
+package com.ruanyh.util.http;
+
+
+public class RequestUtils {
+    /**
+     * 私有的构造方法,不允许实例化
+     */
+    private RequestUtils() {}
+
+
+}
---
+++ src/main/java/com/ruanyh/util/http/ResponseUtils.java
@@ -0,0 +1,11 @@
+package com.ruanyh.util.http;
+
+
+public class ResponseUtils {
+    /**
+     * 私有的构造方法,不允许实例化
+     */
+    private ResponseUtils() {}
+
+
+}
