61bdb2a5b4630d43fe1bd80109da89a1e4ad6028
Ruanyh <ruanyhmail@gmail.com>
2017-03-14 16:53:29 +0800

    commit

---
+++ src/main/java/com/ruanyh/util/http/HttpClientUtils.java
@@ -0,0 +1,10 @@
+package com.ruanyh.util.http;
+
+
+public class HttpClientUtils {
+    /**
+     * 私有的构造方法,不允许实例化
+     */
+    private HttpClientUtils() {}
+
+}
---
+++ src/main/java/com/ruanyh/util/http/OkHttpUtils.java
@@ -0,0 +1,20 @@
+package com.ruanyh.util.http;
+
+import com.squareup.okhttp.OkHttpClient;
+
+import java.util.concurrent.TimeUnit;
+
+public class OkHttpUtils {
+    private static final OkHttpClient HTTP_CLIENT = new OkHttpClient();
+
+    static {
+        HTTP_CLIENT.setConnectTimeout(30, TimeUnit.SECONDS);
+    }
+
+    /**
+     * 私有的构造方法,不允许实例化
+     */
+    private OkHttpUtils() {}
+
+
+}
