4c969729d4f6f27ef340154c0e612feb40371198
Ruanyh <ruanyhmail@gmail.com>
2017-03-21 23:27:01 +0800

    commit

--- src/main/java/com/ruanyh/util/http/HttpClientUtils.java
+++ src/main/java/com/ruanyh/util/http/HttpClientUtils.java
@@ -2,7 +2,6 @@ package com.ruanyh.util.http;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -44,7 +43,7 @@ public class HttpClientUtils {
      * @param url
      * @return
      */
-    public static String get(String url) {
+    public static String get(String url) throws IOException {
         HttpGet httpGet = new HttpGet(url);
         return response(httpGet);
     }
@@ -56,7 +55,7 @@ public class HttpClientUtils {
      * @return
      * @throws URISyntaxException
      */
-    public static String get(String url, Map<String, Object> params) throws URISyntaxException {
+    public static String get(String url, Map<String, Object> params) throws URISyntaxException, IOException {
         URIBuilder uriBuilder = new URIBuilder();
         uriBuilder.setPath(url);
 
@@ -76,11 +75,11 @@ public class HttpClientUtils {
      * @throws URISyntaxException
      */
     public static String get(String url, Map<String, Object> headers, Map<String, Object> params)
-            throws URISyntaxException {
+            throws URISyntaxException, IOException {
         URIBuilder uriBuilder = new URIBuilder();
         uriBuilder.setPath(url);
 
-        ArrayList<NameValuePair> pairs = covertParams2NVPS(params);
+        List<NameValuePair> pairs = covertParams2NVPS(params);
         uriBuilder.setParameters(pairs);
 
         HttpGet httpGet = new HttpGet(uriBuilder.build());
@@ -95,7 +94,7 @@ public class HttpClientUtils {
      * @param url
      * @return
      */
-    public static String post(String url) {
+    public static String post(String url) throws IOException {
         HttpPost httpPost = new HttpPost(url);
         return response(httpPost);
     }
@@ -107,9 +106,9 @@ public class HttpClientUtils {
      * @return
      * @throws UnsupportedEncodingException
      */
-    public static String post(String url, Map<String, Object> params) throws UnsupportedEncodingException {
+    public static String post(String url, Map<String, Object> params) throws IOException {
         HttpPost httpPost = new HttpPost(url);
-        ArrayList<NameValuePair> pairs = covertParams2NVPS(params);
+        List<NameValuePair> pairs = covertParams2NVPS(params);
         httpPost.setEntity(new UrlEncodedFormEntity(pairs, UTF8));
         return response(httpPost);
     }
@@ -123,14 +122,14 @@ public class HttpClientUtils {
      * @throws UnsupportedEncodingException
      */
     public static String post(String url, Map<String, Object> headers, Map<String, Object> params)
-            throws UnsupportedEncodingException {
+            throws IOException {
         HttpPost httpPost = new HttpPost(url);
 
         for (Map.Entry<String, Object> param : headers.entrySet()) {
             httpPost.addHeader(param.getKey(), String.valueOf(param.getValue()));
         }
 
-        ArrayList<NameValuePair> pairs = covertParams2NVPS(params);
+        List<NameValuePair> pairs = covertParams2NVPS(params);
         httpPost.setEntity(new UrlEncodedFormEntity(pairs, UTF8));
 
         return response(httpPost);
@@ -141,8 +140,8 @@ public class HttpClientUtils {
      * @param params
      * @return
      */
-    private static ArrayList<NameValuePair> covertParams2NVPS(Map<String, Object> params) {
-        ArrayList<NameValuePair> pairs = new ArrayList<>();
+    private static List<NameValuePair> covertParams2NVPS(Map<String, Object> params) {
+        List<NameValuePair> pairs = new ArrayList<>();
         for (Map.Entry<String, Object> param : params.entrySet()) {
             pairs.add(new BasicNameValuePair(param.getKey(), String.valueOf(param.getValue())));
         }
@@ -154,25 +153,22 @@ public class HttpClientUtils {
      * @param request
      * @return
      */
-    private static String response(HttpRequestBase request) {
+    private static String response(HttpRequestBase request) throws IOException {
         CloseableHttpClient httpClient = getHttpClient();
+        CloseableHttpResponse response = httpClient.execute(request);
         try {
-            CloseableHttpResponse response = httpClient.execute(request);
+            response = httpClient.execute(request);
             // response.getStatusLine().getStatusCode();
             HttpEntity entity = response.getEntity();
             if (entity != null) {
                 // long len = entity.getContentLength();// -1 表示长度未知
                 String result = EntityUtils.toString(entity);
-                response.close();
+
                 // httpClient.close();
                 return result;
             }
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         } finally {
-
+            response.close();
         }
         return EMPTY_STR;
     }
