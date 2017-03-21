4ffc737cfafa0e20295238573e072bab95b596b4
Ruanyh <ruanyhmail@gmail.com>
2017-03-21 23:04:12 +0800

    commit

--- src/main/java/com/ruanyh/util/common/FtpUtils.java
+++ src/main/java/com/ruanyh/util/common/FtpUtils.java
@@ -1,13 +1,18 @@
 package com.ruanyh.util.common;
 
 
+import org.apache.commons.net.ftp.FTPClient;
+
 public class FtpUtils {
+
+
     /**
      * 私有的构造方法,不允许实例化
      */
     private FtpUtils() {}
 
     public static void upload(String filePath) {
+        FTPClient client = new FTPClient();
 
     }
 
--- src/main/java/com/ruanyh/util/http/HttpClientUtils.java
+++ src/main/java/com/ruanyh/util/http/HttpClientUtils.java
@@ -1,10 +1,144 @@
 package com.ruanyh.util.http;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 
 public class HttpClientUtils {
+    private static PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+    private static String EMPTY_STR = "";
+    private static String UTF8 = "UTF-8";
+
+    static {
+        connectionManager.setMaxTotal(100);                 // 整个连接池最大连接数
+        connectionManager.setDefaultMaxPerRoute(5);         // 每路由最大连接数，默认值是2
+    }
+
     /**
      * 私有的构造方法,不允许实例化
      */
     private HttpClientUtils() {}
 
+    /**
+     * 通过连接池获取HttpClient
+     * @return
+     */
+    private static CloseableHttpClient getHttpClient() {
+        return HttpClients.custom().setConnectionManager(connectionManager).build();
+    }
+
+    /**
+     * Get方法
+     * @param url
+     * @return
+     */
+    public static String get(String url) {
+        HttpGet httpGet = new HttpGet(url);
+        return response(httpGet);
+    }
+
+    public static String get(String url, Map<String, Object> params) throws URISyntaxException {
+        URIBuilder uriBuilder = new URIBuilder();
+        uriBuilder.setPath(url);
+
+        List<NameValuePair> pairs = covertParams2NVPS(params);
+        uriBuilder.setParameters(pairs);
+
+        HttpGet httpGet = new HttpGet(uriBuilder.build());
+        return response(httpGet);
+    }
+
+    public static String get(String url, Map<String, Object> headers, Map<String, Object> params)
+            throws URISyntaxException {
+        URIBuilder uriBuilder = new URIBuilder();
+        uriBuilder.setPath(url);
+
+        ArrayList<NameValuePair> pairs = covertParams2NVPS(params);
+        uriBuilder.setParameters(pairs);
+
+        HttpGet httpGet = new HttpGet(uriBuilder.build());
+        for (Map.Entry<String, Object> param : headers.entrySet()) {
+            httpGet.addHeader(param.getKey(), String.valueOf(param.getValue()));
+        }
+        return response(httpGet);
+    }
+
+
+    public static String post(String url) {
+        HttpPost httpPost = new HttpPost(url);
+        return response(httpPost);
+    }
+
+    public static String post(String url, Map<String, Object> params) throws UnsupportedEncodingException {
+        HttpPost httpPost = new HttpPost(url);
+        ArrayList<NameValuePair> pairs = covertParams2NVPS(params);
+        httpPost.setEntity(new UrlEncodedFormEntity(pairs, UTF8));
+        return response(httpPost);
+    }
+
+    public static String post(String url, Map<String, Object> headers, Map<String, Object> params)
+            throws UnsupportedEncodingException {
+        HttpPost httpPost = new HttpPost(url);
+
+        for (Map.Entry<String, Object> param : headers.entrySet()) {
+            httpPost.addHeader(param.getKey(), String.valueOf(param.getValue()));
+        }
+
+        ArrayList<NameValuePair> pairs = covertParams2NVPS(params);
+        httpPost.setEntity(new UrlEncodedFormEntity(pairs, UTF8));
+
+        return response(httpPost);
+    }
+
+    private static ArrayList<NameValuePair> covertParams2NVPS(Map<String, Object> params) {
+        ArrayList<NameValuePair> pairs = new ArrayList<>();
+        for (Map.Entry<String, Object> param : params.entrySet()) {
+            pairs.add(new BasicNameValuePair(param.getKey(), String.valueOf(param.getValue())));
+        }
+        return pairs;
+    }
+
+
+    private static String response(HttpRequestBase request) {
+        // CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpClient httpClient = getHttpClient();
+        try {
+            CloseableHttpResponse response = httpClient.execute(request);
+            // response.getStatusLine().getStatusCode();
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                // long len = entity.getContentLength();// -1 表示长度未知
+                String result = EntityUtils.toString(entity);
+                response.close();
+                // httpClient.close();
+                return result;
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+
+        }
+        return EMPTY_STR;
+    }
 }
