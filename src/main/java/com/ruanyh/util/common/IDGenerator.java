6a8ab6d0affc79654fb4718f44688726c7b3b0fa
Ruanyh <ruanyhmail@gmail.com>
2017-09-10 09:15:58 +0800

    commit

--- src/main/java/com/ruanyh/util/common/IDGenerator.java
+++ src/main/java/com/ruanyh/util/common/IDGenerator.java
@@ -1,5 +1,6 @@
 package com.ruanyh.util.common;
 
+import java.util.Random;
 import java.util.UUID;
 
 /**
@@ -21,14 +22,32 @@ public class IDGenerator {
         return UUID.randomUUID().toString().replace("-", "");
     }
 
-    public static String randomCode() {
-        return null;
+    /**
+     * 数字随机码
+     * 6位
+     * @return
+     */
+    public static String randomNum() {
+        return randomNum(6);
     }
 
-    public static String randomCode(int n) {
-        return null;
+    /**
+     * 数字随机码
+     * @param n 位数
+     * @return
+     */
+    public static String randomNum(int n) {
+        StringBuffer code = new StringBuffer();
+        for (int i = 0; i < n; i++) {
+            code.append(new Random().nextInt(10));
+        }
+        return code.toString();
     }
 
 
+    public static void main(String[] args) {
+        System.out.println(uuid());
+        System.out.println(randomNum(6));
+    }
 
 }
--- src/main/java/com/ruanyh/util/common/PathUtils.java
+++ src/main/java/com/ruanyh/util/common/PathUtils.java
@@ -1,6 +1,8 @@
 package com.ruanyh.util.common;
 
-
+/**
+ * 路径工具类
+ */
 public class PathUtils {
     /**
      * 私有的构造方法,不允许实例化
